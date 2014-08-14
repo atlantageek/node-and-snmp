f9e3f4a247c0b753a443a02f21918a335f4eb74d
atlantageek <atlantageek@gmail.com>
2014-08-14 14:00:14 -0400

    Making sure everything is committed

--- code/snmp_listener/snmptrapd.js
+++ code/snmp_listener/snmptrapd.js
@@ -1,7 +1,21 @@
 var os = require('os');
 var snmp = require('snmpjs');
+var http = require('http');
+var express = require('express');
+var app = express();
 
 var bunyan = require('bunyan');
+var result=[];
+
+app.use(express.static('public'));
+app.get('/get_today_count', function(req, res) {
+   console.log(result.length);
+  res.send(result.length.toString());
+});
+
+var server = app.listen(3001, function() {
+  console.log('Listening on port %d', server.address().port);
+});
 
 var log = new bunyan({
       name: 'snmpd',
@@ -11,7 +25,10 @@ var log = new bunyan({
 var trapd = snmp.createTrapListener({log: log});
 
 trapd.on('trap', function(msg){
-      console.log('trap detected');
-});
+       result.push(msg);
+   var now = new Date();
+   console.log("Trap Received " + now);
+   console.log(result.length);
+   });
 
 trapd.bind({family: 'udp4', port: 162});
